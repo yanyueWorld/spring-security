00330f982d66537bf232a16f1c9600649904da4f
yanyue <1361557116@qq.com>
2019-07-04 14:26:42 +0800

    Update spring-security.sql
    update sql

--- demo02/spring-security.sql
+++ demo02/spring-security.sql
@@ -34,10 +34,10 @@ INSERT INTO `role` VALUES (1, 'ROLE_admin');
 INSERT INTO `role` VALUES (2, 'ROLE_user');
 
 -- ----------------------------
--- Table structure for user
+-- Table structure for account
 -- ----------------------------
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user`  (
+DROP TABLE IF EXISTS `account`;
+CREATE TABLE `account`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
@@ -45,16 +45,16 @@ CREATE TABLE `user`  (
 ) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of user
+-- Records of account
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 'admin', '$2a$10$TCEPRjCsnoaUr46QvqfU8uSCKBh26iJn9Dm0TgJzdgPHkNuZm4hGC');
-INSERT INTO `user` VALUES (2, 'user', '$2a$10$vOsAopRZ0kg6eFSt0pWEY.EuduRcnSZUdlyCAlkug.9h5klrZmiXG');
+INSERT INTO `account` VALUES (1, 'admin', '$2a$10$TCEPRjCsnoaUr46QvqfU8uSCKBh26iJn9Dm0TgJzdgPHkNuZm4hGC');
+INSERT INTO `account` VALUES (2, 'user', '$2a$10$vOsAopRZ0kg6eFSt0pWEY.EuduRcnSZUdlyCAlkug.9h5klrZmiXG');
 
 -- ----------------------------
 -- Table structure for user_role
 -- ----------------------------
-DROP TABLE IF EXISTS `user_role`;
-CREATE TABLE `user_role`  (
+DROP TABLE IF EXISTS `account_role`;
+CREATE TABLE `account_role`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `role_id` int(11) NOT NULL,
@@ -64,8 +64,8 @@ CREATE TABLE `user_role`  (
 -- ----------------------------
 -- Records of user_role
 -- ----------------------------
-INSERT INTO `user_role` VALUES (1, 1, 1);
-INSERT INTO `user_role` VALUES (2, 1, 2);
-INSERT INTO `user_role` VALUES (3, 2, 2);
+INSERT INTO `account_role` VALUES (1, 1, 1);
+INSERT INTO `account_role` VALUES (2, 1, 2);
+INSERT INTO `account_role` VALUES (3, 2, 2);
 
 SET FOREIGN_KEY_CHECKS = 1;
